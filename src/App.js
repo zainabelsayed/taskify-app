4e4ff796dacec531a26bc2fb69439a69b6d92d7b
Roaa Dahy <roaadahy1@gmail.com>
2021-11-20 17:54:52 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import RegisterForm from './components/register/RegisterForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <RegisterForm></RegisterForm>
+   
+    // <Router>
+    //   <Switch>
+    //     <Route component={RegisterForm} path="/" />
+    //     {/* <Route component={LoginForm} path="/" /> */}
+    //   </Switch>
+    // </Router>
   );
 }
 
